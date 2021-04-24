08392543083131bc687b44d02f699a76cb9d43fb
Graziele Dantas <elentary@gmail.com>
2021-04-24 18:21:52 -0300

    Update API

---
+++ src/components/DrinkCategories/AlcoholicDrinks.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+class AlcoholicDrinks extends Component {
+  state = {
+    drinks: [],
+  };
+
+  componentDidMount = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+      );
+      console.log(response);
+      this.setState({ drinks: [...response.data.drinks] });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  render() {
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-around m-5 wrap flex-wrap">
+          {this.state.drinks.map((drink) => {
+            return (
+              <div key={drink.idDrink} className="card bg-dark text-white">
+                <img
+                  src={drink.strDrinkThumb}
+                  className="card-img"
+                  style={{ width: "300px" }}
+                  alt={drink.strDrink}
+                />
+                <div className="card-img-overlay">
+                  <h4
+                    style={{
+                      textShadow: "2px 4px 3px #FF2222",
+                    }}
+                    className="card-title d-flex justify-content-center text-center"
+                  >
+                    {drink.strDrink}
+                  </h4>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AlcoholicDrinks;
