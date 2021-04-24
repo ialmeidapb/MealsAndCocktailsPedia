58d8a6bf7987b786b3dcfeb7c375960ecc145a76
Graziele Dantas <elentary@gmail.com>
2021-04-24 16:16:55 -0300

    Start

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+
+
+    </div>
+  );
+}
+
+export default App;
