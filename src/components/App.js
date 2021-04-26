eca9fd349ea7ccde7162a673ac2c6274091d8391
ialmeida <ialmeida.pb@gmail.com>
2021-04-26 16:11:52 -0300

    pages organized

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Drinks from "./DrinkCategories/Drinks"
 import Meals from "./FoodCategories/Meals"
-
+import FoodHomepage from "./Homepages/FoodHomepage"
+import DrinkHomepage from "./Homepages/DrinkHomepage"
+import DrinkDetail from "./DrinkCategories/DrinkDetail"
+import FoodDetail from "./FoodCategories/FoodDetail"
 
 
 
@@ -12,8 +15,14 @@ function App() {
     
     <div className="App">
   <BrowserRouter>
-  <Route exact path="/:id" component={Meals} />
-  <Route exact path="/:id" component={Drinks} />
+  
+  <Route exact path="/foods" component={FoodHomepage} />
+  <Route exact path="/foods/:id" component={Meals} />
+  <Route exact path="/foods/meals/:id" component={FoodDetail} />
+  <Route exact path="/drinks" component={DrinkHomepage} />
+  <Route exact path="/drinks/:id" component={Drinks} />
+  <Route exact path="/drinks/cocktail/:id" component={DrinkDetail} />
+  
 
   </BrowserRouter>
     
---
+++ src/components/DrinkCategories/DrinkDetail.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+
+
+class DrinkDetail extends Component{
+    state = {
+        drink: "",
+      };
+
+      componentDidMount = async () => {
+        console.log(this.props.match.params.code)
+        try {
+          const response = await axios.get(
+            `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${this.props.match.params.id}`
+          );
+          console.log(response)
+          this.setState({ drink: { ...response.data.drinks[0] } });
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+    render=()=>{
+        return(
+            <div>
+           
+            <div className="container mt-5 text-center">
+            <img src={this.state.drink.strDrinkThumb} width="100px"></img>
+            <h1>{this.state.drink.strDrink}</h1>
+            <h4>{this.state.drink.strGlass} &nbsp; {this.state.drink.strIBA} </h4>
+            <h5>{this.state.drink.attenuation_level} </h5>
+            <p className="text-justify">{this.state.drink.strInstructions}</p>
+            
+            </div>
+
+
+
+            </div>
+        )
+    }
+}
+
+export default DrinkDetail;
