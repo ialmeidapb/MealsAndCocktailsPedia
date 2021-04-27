08f9a629a4e23b554cc30ea6ef26a0fe75bea476
ialmeida <ialmeida.pb@gmail.com>
2021-04-27 20:24:22 -0300

    print function added

---
+++ src/components/ComponentToPrint.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactToPrint from 'react-to-print';
+import FoodDetail from "./FoodCategories/FoodDetail"
+
+
+
+
+class ComponentToPrint extends React.PureComponent {
+    render() {
+      return (
+        <div>
+          
+            {/* <h1>{FoodDetail.state.food.strMeal}</h1>
+            <p>{FoodDetail.state.food.strInstructions}</p> */}
+            <h1>Oiiii</h1>
+          
+          
+        </div>
+      );
+    }
+  }
+
+  export default ComponentToPrint;
