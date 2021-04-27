34a95fa1cd9c9c3b00cc9b83bbc6bee27e6b256b
ialmeida <ialmeida.pb@gmail.com>
2021-04-27 13:44:50 -0300

    search bar

--- src/components/DrinkCategories/DrinkDetail.js
+++ src/components/DrinkCategories/DrinkDetail.js
@@ -28,11 +28,26 @@ class DrinkDetail extends Component{
             <div className="container mt-5 text-center">
             <img src={this.state.drink.strDrinkThumb} width="100px"></img>
             <h1>{this.state.drink.strDrink}</h1>
-            <h4>{this.state.drink.strGlass} &nbsp; {this.state.drink.strIBA} </h4>
+            <h4>Recommended Glass: {this.state.drink.strGlass}</h4>
             <h5>{this.state.drink.attenuation_level} </h5>
             <ul>
             {/* <li>{this.state.drink[`strIngredient${indice}`]}</li> */}
-            
+            <li>{this.state.drink.strIngredient1}</li>
+            <li>{this.state.drink.strIngredient2}</li>
+            <li>{this.state.drink.strIngredient3}</li>
+            <li>{this.state.drink.strIngredient4}</li>
+            <li>{this.state.drink.strIngredient5}</li>
+            <li>{this.state.drink.strIngredient6}</li>
+            <li>{this.state.drink.strIngredient7}</li>
+            <li>{this.state.drink.strIngredient8}</li>
+            <li>{this.state.drink.strIngredient9}</li>
+            <li>{this.state.drink.strIngredient10}</li>
+            <li>{this.state.drink.strIngredient11}</li>
+            <li>{this.state.drink.strIngredient12}</li>
+            <li>{this.state.drink.strIngredient13}</li>
+            <li>{this.state.drink.strIngredient14}</li>
+            <li>{this.state.drink.strIngredient15}</li>
+
             </ul>
             <p className="text-justify">{this.state.drink.strInstructions}</p>
             
