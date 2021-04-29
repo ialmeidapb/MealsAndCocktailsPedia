40d89ed3fab057120e39ae4dbbe36174f5154a06
ialmeida <ialmeida.pb@gmail.com>
2021-04-29 10:15:33 -0300

    icon

--- src/components/FoodCategories/Printer.js
+++ src/components/FoodCategories/Printer.js
@@ -13,7 +13,7 @@ class Printer extends React.PureComponent {
           trigger={() => {
             // NOTE: could just as easily return <SomeComponent />. Do NOT pass an `onClick` prop
             // to the root node of the returned component as it will be overwritten.
-            return <a href="#"><i className="fas fa-print"></i>Click here to Print!</a>;
+            return <a href="#"><i className="fas fa-print"></i> PRINT!</a>;
           }}
           content={() => this.componentRef}
         />
